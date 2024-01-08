554e41b36868c1c69a78c47cb20bd04943e4e980
truongnguyendev <nguyenxuantruong.dev@gmail.com>
2024-01-08 16:06:53 +0700

    Update RTMPView.swift

--- ios/RTMPManager/RTMPView.swift
+++ ios/RTMPManager/RTMPView.swift
@@ -86,6 +86,7 @@ class RTMPView: UIView {
               onConnectionSuccess!(nil)
             }
            changeStreamState(status: "CONNECTING")
+           RTMPCreator.stream.publish(streamName as String)
            break
        
        case RTMPConnection.Code.connectFailed.rawValue:
